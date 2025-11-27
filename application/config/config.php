92f336ec6d096abcc844c364e9b31ad3643b1766
kbhargavi-dev <kelavathubhargavi@gmail.com>
2025-11-27 16:55:36 +0530

    base url change

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://localhost/suzuki/';
+$config['base_url'] = 'http://localhost/padmajasuzuki/';
 
 
 
