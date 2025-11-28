9bddf9edd3c57bde6bdbb5e38a00cd7a3f4f5e00
vsravani-dev <sravani87878@gmail.com>
2025-11-28 18:23:46 +0530

    banner fixed for mobile screen

--- application/views/templates/home/index.php
+++ application/views/templates/home/index.php
@@ -1,30 +1,43 @@
-        <!-- banner-section -->
-        <section class="banner-section p_relative pb_80">
-            <div class="banner-carousel owl-theme owl-carousel owl-nav-none dots-style-one">
-                <div class="slide-item" style="position:relative; width:100%; height:75vh; overflow:hidden;">
-                    <div class="bg-layer" 
-                         style="position:absolute; top:0; left:0; width:100%; height:100%; 
-                                background-image:url('assets/images/banner/suzuki.webp'); 
-                                background-size:cover; background-position:center; background-repeat:no-repeat;">
-                    </div>
-                </div>
+     <style>
 
-               <div class="slide-item" style="position:relative; width:100%; height:75vh; overflow:hidden;">
-                    <div class="bg-layer" 
-                         style="position:absolute; top:0; left:0; width:100%; height:100%; 
-                                background-image:url('assets/images/banner/suzuki2.webp'); 
-                                background-size:cover; background-position:center; background-repeat:no-repeat;">
-                    </div>
-                </div>
-               <div class="slide-item" style="position:relative; width:100%; height:75vh; overflow:hidden;">
-                    <div class="bg-layer" 
-                         style="position:absolute; top:0; left:0; width:100%; height:100%; 
-                                background-image:url('assets/images/banner/suzuki3.webp'); 
-                                background-size:cover; background-position:center; background-repeat:no-repeat;">
-                    </div>
-                </div>
-            </div>
-        </section>
+@media (max-width: 768px) {
+
+    .banner-carousel .slide-item:nth-child(1) .bg-layer {
+        background-image: url('assets/images/banner/mobile.webp') !important;
+    }
+
+    .banner-carousel .slide-item:nth-child(2) .bg-layer {
+        background-image: url('assets/images/banner/mobile.webp') !important;
+    }
+
+    .banner-carousel .slide-item:nth-child(3) .bg-layer {
+        background-image: url('assets/images/banner/mobile.webp') !important;
+    }
+
+    /* Adjust banner height for mobile */
+    .banner-carousel .slide-item {
+        height: 70vh !important;
+    }
+}
+     </style>
+     <!-- banner-section -->
+       <section class="banner-section p_relative pb_80">
+    <div class="banner-carousel owl-theme owl-carousel owl-nav-none dots-style-one">
+
+        <div class="slide-item">
+            <div class="bg-layer" style="background-image:url('assets/images/banner/suzuki.webp');"></div>
+        </div>
+
+        <div class="slide-item">
+            <div class="bg-layer" style="background-image:url('assets/images/banner/suzuki2.webp');"></div>
+        </div>
+
+        <div class="slide-item">
+            <div class="bg-layer" style="background-image:url('assets/images/banner/suzuki3.webp');"></div>
+        </div>
+
+    </div>
+</section>
         <!-- banner-section end -->
 
         <section class="search-section">
